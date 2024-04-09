8f7efcc165db55fd37c70527752b21bfddf82a4b
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-04-09 14:38:56 +0200

    chore: merge walk node fixes from proper branch

--- pkg/manifest/mantaray/walker.go
+++ pkg/manifest/mantaray/walker.go
@@ -18,42 +18,6 @@ func walkNodeFnCopyBytes(path []byte, node *Node, walkFn WalkNodeFunc) error {
 }
 
 // walkNode recursively descends path, calling walkFn.
-// visits the nodes in sequence
-func walkNodeInSequence(ctx context.Context, path []byte, l Loader, n *Node, walkFn WalkNodeFunc) error {
-	if n.forks == nil {
-		if err := n.load(ctx, l); err != nil {
-			return err
-		}
-	}
-
-	err := walkNodeFnCopyBytes(ctx, path, n, nil, walkFn)
-	if err != nil {
-		return err
-	}
-
-	// Extract and sort the keys.
-	keys := make([]byte, 0, len(n.forks))
-	for k := range n.forks {
-		keys = append(keys, k)
-	}
-
-	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
-
-	for _, k := range keys {
-		v := n.forks[k]
-		nextPath := append(path[:0:0], path...)
-		nextPath = append(nextPath, v.prefix...)
-
-		err := walkNodeInSequence(ctx, nextPath, l, v.Node, walkFn)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// walkNode recursively descends path, calling walkFn.
 func walkNode(ctx context.Context, path []byte, l Loader, n *Node, walkFn WalkNodeFunc) error {
 	if n.forks == nil {
 		if err := n.load(ctx, l); err != nil {
@@ -89,15 +53,10 @@ func walkNode(ctx context.Context, path []byte, l Loader, n *Node, walkFn WalkNo
 // WalkNode walks the node tree structure rooted at root, calling walkFn for
 // each node in the tree, including root. All errors that arise visiting nodes
 // are filtered by walkFn.
-func (n *Node) WalkNode(ctx context.Context, root []byte, l Loader, walkFn WalkNodeFunc, shouldSort bool) error {
+func (n *Node) WalkNode(ctx context.Context, root []byte, l Loader, walkFn WalkNodeFunc) error {
 	node, err := n.LookupNode(ctx, root, l)
 	if err != nil {
 		return walkFn(root, nil, err) // Directly return after handling error.
 	}
-
-	if shouldSort {
-		return walkNodeInSequence(ctx, root, l, node, walkFn) // Handle sorted walking.
-	}
-
 	return walkNode(ctx, root, l, node, walkFn)
 }
